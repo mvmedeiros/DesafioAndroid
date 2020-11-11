eac45cc3594686eaa5272466c44419e75bf5ccf6
mvmedeiros <mvbmedeiros@gmail.com>
2020-11-10 22:26:18 -0300

    Update inicial da tela de splash.

---
+++ app/src/main/java/com/example/desafiomobileandroid/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.desafiomobileandroid
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.TextView
+
+const val DURACAO_DO_SPLASH = 4000L // L de Long
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Inicia a MainActivity após 4 segundos
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, DURACAO_DO_SPLASH)
+    }
+
+}
