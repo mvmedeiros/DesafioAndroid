a237ccd7eda699e391eb5c312274cf408dc633e6
mvmedeiros <mvbmedeiros@gmail.com>
2020-11-11 23:05:45 -0300

    Atualiza navegação entre telas de sign in e registro.

--- app/src/main/java/com/example/desafiomobileandroid/mainactivity/view/MainActivity.kt
+++ app/src/main/java/com/example/desafiomobileandroid/mainactivity/view/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.desafiomobileandroid.mainactivity.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.desafiomobileandroid.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.signInFragment)
+            .build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/desafiomobileandroid/signin/view/SignInFragment.kt
+++ app/src/main/java/com/example/desafiomobileandroid/signin/view/SignInFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.os.bundleOf
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.desafiomobileandroid.R
 
@@ -21,8 +22,14 @@ class SignInFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val navController = Navigation.findNavController(view)
 
+        register(view, navController)
+    }
+
+    private fun register(view: View, navController: NavController) {
+        view.findViewById<Button>(R.id.btnSignInRegister).setOnClickListener {
+            navController.navigate(R.id.action_signInFragment_to_registerFragment)
+        }
     }
 }
